7ee5c3d0e5f7f4bfa9b5dbb0189ee28d11a902b8
Mahitha JV <mahithajv495@gmail.com>
2025-09-16 22:12:28 +0530

    Create singleElementinSortedarray.cpp

---
+++ singleElementinSortedarray.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int start=0;
+        int end=nums.size()-1;
+        while(start<end){
+            int mid=start+(end-start)/2;
+            if(nums[mid]!=nums[mid+1] && nums[mid]!=nums[mid-1])
+            return nums[mid];
+            else{
+                if(mid%2==0){
+                    if(nums[mid]==nums[mid-1])
+                    end=mid-1;
+                    else
+                    start=mid+1;
+                }
+                else{
+                    if(nums[mid]==nums[mid+1])
+                    start=mid+1;
+                    else
+                    end=mid-1;
+                }
+                return nums[mid];
+            }
+        }
+        
+    }
+};
